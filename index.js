f3b32767caa85541db705a1b662eb5673186a871
David <david@scrib-art.fr>
2024-09-08 17:21:58 +0200

    add style css & forms

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const calendar = new VanillaCalendar({
   selector: "#myCalendar",
   onSelect: (data, elem) => {
     console.log(data);
-    consiole.log(elem);
   },
+  months: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Aout",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Decembre",
+  ],
+  shortWeekday: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
 });
+
+let selectedDate = {};
